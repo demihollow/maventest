4c5d248f6026bc1b2c60a13ba12acb1815a0e8da
demihollow <mitzuti@web.de>
2016-11-29 16:38:25 +0100

    Add files via upload

---
+++ src/main/java/de/Kuhnert/mavenbeispiel.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class mavenbeispiel {
+
+	public static void main(String args[]){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Texteingabe:");
+		String s = scanner.nextLine();
+		System.out.println(s.toUpperCase());
+	}
+}
